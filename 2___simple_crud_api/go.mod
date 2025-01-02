cefc761a8b3693d4b636b4cac9036b4a16c351f6
serediukit <serediukit@gmail.com>
2025-01-03 01:53:01 +0200

    create structs

--- 2___simple_crud_api/go.mod
+++ 2___simple_crud_api/go.mod
@@ -1,3 +1,5 @@
 module 2___simple_crud_api
 
 go 1.23
+
+require github.com/gorilla/mux v1.8.1 // indirect
---
+++ 2___simple_crud_api/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+var movies []Movie
+
+func main() {
+
+}
