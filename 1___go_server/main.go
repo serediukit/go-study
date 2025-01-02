8346c23db406978e57817361a15ad477e2a2cce2
serediukit <serediukit@gmail.com>
2025-01-03 01:45:40 +0200

    complete simple server

--- 1___go_server/main.go
+++ 1___go_server/main.go
@@ -6,6 +6,43 @@ import (
 	"net/http"
 )
 
-func main() {
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		_, _ = fmt.Fprintf(w, "ParseForm() err: %v\n", err)
+		return
+	}
 
+	_, _ = fmt.Fprintf(w, "Form request proccesing\n")
+
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	_, _ = fmt.Fprintf(w, "Name = %s\n", name)
+	_, _ = fmt.Fprintf(w, "Address = %s\n", address)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed.", http.StatusMethodNotAllowed)
+	}
+
+	_, err := fmt.Fprintf(w, "Hello\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
