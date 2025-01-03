08548ce19c6f16765765347b641a10410f2f1a21
serediukit <serediukit@gmail.com>
2025-01-04 01:43:40 +0200

    complete controller

--- 3___go_my_sql/cmd/main/main.go
+++ 3___go_my_sql/cmd/main/main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/serediukit/go-study/3___go_my_sql/pkg/routes"
+	"log"
+	"net/http"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":9010", r))
+}
