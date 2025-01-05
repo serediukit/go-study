63eca0c9d9e30795a97a6e9934bbea57a1ce610a
serediukit <serediukit@gmail.com>
2025-01-06 00:42:34 +0200

    add Invoice structure

--- p_1_restaurant_backend/models/foodModel.go
+++ p_1_restaurant_backend/models/foodModel.go
@@ -6,12 +6,12 @@ import (
 )
 
 type Food struct {
-	ID        primitive.ObjectID `json:"_id" bson:"_id"`
-	Name      *string            `json:"name" bson:"name" validate:"required,min=2,max=100"`
-	Price     *float64           `json:"price" bson:"price" validate:"required"`
-	FoodImage *string            `json:"food_image" bson:"food_image" validate:"required"`
-	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
-	UpdatedAt time.Time          `json:"updated_at" bson:"updated_at"`
-	FoodID    string             `json:"food_id" bson:"food_id"`
-	MenuID    *string            `json:"menu_id" bson:"menu_id" validate:"required"`
+	ID        primitive.ObjectID `bson:"_id"`
+	Name      *string            `json:"name" validate:"required,min=2,max=100"`
+	Price     *float64           `json:"price" validate:"required"`
+	FoodImage *string            `json:"food_image" validate:"required"`
+	CreatedAt time.Time          `json:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at"`
+	FoodID    string             `json:"food_id"`
+	MenuID    *string            `json:"menu_id" validate:"required"`
 }
