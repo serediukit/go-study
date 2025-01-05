69f22ba5dc98844591d376d86ea789a66d39a902
serediukit <serediukit@gmail.com>
2025-01-05 02:08:53 +0200

    add user routes

--- p_1_restaurant_backend/main.go
+++ p_1_restaurant_backend/main.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"os"
+	"restaurant_backend/database"
+	"restaurant_backend/middleware"
+	"restaurant_backend/routes"
+)
 
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	routes.UserRoutes(router)
+	router.Use(middleware.Authentication())
+
+	routes.FoodRoutes(router)
+	routes.MenuRoutes(router)
+	routes.TableRoutes(router)
+	routes.OrderRoutes(router)
+	routes.OrderItemRoutes(router)
+	routes.InvoiceRoutes(router)
+
+	fmt.Println("Server started on port " + port)
+	err := router.Run(":" + port)
+	if err != nil {
+		panic(err)
+	}
 }
--- p_1_restaurant_backend/routes/userRouter.go
+++ p_1_restaurant_backend/routes/userRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"restaurant_backend/controllers"
+)
+
+func UserRoutes(router *gin.Engine) {
+	router.GET("/users", controllers.GetUsers())
+	router.GET("/users/:user_id", controllers.GetUser())
+	router.POST("/users/signup", controllers.SignUp())
+	router.POST("/users/login", controllers.Login())
+}
