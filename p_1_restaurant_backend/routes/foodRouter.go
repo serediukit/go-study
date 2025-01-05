e0183d9ef1c498782be2764719746695aa39a889
serediukit <serediukit@gmail.com>
2025-01-05 02:12:44 +0200

    add food routes

--- p_1_restaurant_backend/routes/foodRouter.go
+++ p_1_restaurant_backend/routes/foodRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"restaurant_backend/controllers"
+)
+
+func FoodRoutes(router *gin.Engine) {
+	router.GET("/foods", controllers.GetFoods())
+	router.GET("/foods/:food_id", controllers.GetFood())
+	router.GET("/foods", controllers.CreateFood())
+	router.PATCH("/foods/:food_id", controllers.UpdateFood())
+}
