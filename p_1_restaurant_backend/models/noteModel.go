dc24190b54d79c2046509df9464a53fd4428feee
serediukit <serediukit@gmail.com>
2025-01-07 22:20:59 +0200

    add Note structure

--- p_1_restaurant_backend/models/noteModel.go
+++ p_1_restaurant_backend/models/noteModel.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Note struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	Text      string             `json:"text"`
+	Title     string             `json:"title"`
+	CreatedAt time.Time          `json:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at"`
+	NoteID    string             `json:"note_id"`
+}
