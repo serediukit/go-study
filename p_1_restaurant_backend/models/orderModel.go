9e7d923d7c8f77cb31cb398cfd429ca785fab38d
serediukit <serediukit@gmail.com>
2025-01-07 22:11:42 +0200

    add Order structure

--- p_1_restaurant_backend/models/orderModel.go
+++ p_1_restaurant_backend/models/orderModel.go
@@ -1,1 +1,15 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type Order struct {
+	ID        primitive.ObjectID `bson:"_id"`
+	OrderDate time.Time          `json:"order_date" validate:"required"`
+	CreatedAt time.Time          `json:"created_at"`
+	UpdatedAt time.Time          `json:"updated_at"`
+	OrderID   string             `json:"order_id"`
+	TableID   *string            `json:"table_id" validate:"required"`
+}
