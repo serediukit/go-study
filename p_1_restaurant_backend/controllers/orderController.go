0c97f764988663fd18185aa6947e631492f9c4de
serediukit <serediukit@gmail.com>
2025-01-10 23:49:12 +0200

    complete GetOrders func

--- p_1_restaurant_backend/controllers/orderController.go
+++ p_1_restaurant_backend/controllers/orderController.go
@@ -1,12 +1,34 @@
 package controllers
 
 import (
+	"context"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"log"
+	"net/http"
+	"restaurant_backend/database"
+	"time"
 )
+
+var orderCollection *mongo.Collection = database.OpenCollection(database.Client, "order")
 
 func GetOrders() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		result, err := orderCollection.Find(context.TODO(), bson.M{})
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occurred while listing the order items"})
+		}
+
+		var allOrders []bson.M
+		if err = result.All(ctx, &allOrders); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allOrders)
 	}
 }
 
